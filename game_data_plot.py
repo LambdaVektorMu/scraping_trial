bfbc213a79bcaf80265830aa849b9f463570b9dd
l_vektor_m <lambdavektormuwork@gmail.com>
2021-08-17 10:06:31 +0900

    2021年8月17日のコミット（目立った更新はない）

--- game_data_plot.py
+++ game_data_plot.py
@@ -1,9 +1,11 @@
 #!/home/l_vektor_m/venv3.8/bin/python
 # coding: utf-8
 
+import matplotlib
 import matplotlib.pyplot as plot
 import pandas
 
+#matplotlib.rc('font', family='Noto Sans CJK JP')
 # csvファイルをデータフレームに格納する
 enc = 'utf-8-sig'
 csv_title_game_price_value = 'ルデヤでの各ゲーム機の買取値段推移（値のみ）.csv'
@@ -19,7 +21,7 @@ plot.plot(data_frame['date']+data_frame['hour'], data_frame['PlayStation5 DE'], 
 # 日付（x軸）の目盛り表示を見やすくする
 xlabel_list = (data_frame['date']+data_frame['hour']).to_list()
 # 日本語を抜いて簡略表示
-plot_xlabel_list = [l.replace('2021年', '').replace('月', '/').replace('日', '/').strip('時') for l in xlabel_list]
+plot_xlabel_list = [l.replace('2021年', '').replace('月', '/').replace('日', ' ') for l in xlabel_list]
 # 12時間毎に表示
 print(len(plot_xlabel_list), plot_xlabel_list)
 days = len(plot_xlabel_list)//12
@@ -28,17 +30,19 @@ days_plot = [d*12+3 for d in range(days)]
 print(days_plot)
 label_list = [plot_xlabel_list[days_plot[ll]] for ll in range(days) if days_plot[ll] < len(plot_xlabel_list)]
 print(label_list)
-plot.xticks(days_plot, label_list, fontsize=8)
+FONT_SIZE=7
+plot.xticks(days_plot, label_list, fontsize=FONT_SIZE)
 plot.legend()
 FIG_W = 12
 fig.set_figwidth(FIG_W)
 
+plot.title('ルデヤ買取価格')
 plot.savefig('rudeya_PS5BR-PS5DE.png')
 plot.close()
 
 fig = plot.figure()
 plot.plot(data_frame['date']+data_frame['hour'], data_frame['PlayStation5'], '.-b', label='PS5')
-plot.xticks(days_plot, label_list, fontsize=8)
+plot.xticks(days_plot, label_list, fontsize=FONT_SIZE)
 plot.legend()
 fig.set_figwidth(FIG_W)
 plot.savefig('rudeya_PS5BR.png')
@@ -46,7 +50,7 @@ plot.close()
 
 fig = plot.figure()
 plot.plot(data_frame['date']+data_frame['hour'], data_frame['PlayStation5 DE'], '.-g', label='PS5DE')
-plot.xticks(days_plot, label_list, fontsize=8)
+plot.xticks(days_plot, label_list, fontsize=FONT_SIZE)
 plot.legend()
 fig.set_figwidth(FIG_W)
 plot.savefig('rudeya_PS5DE.png')
@@ -54,7 +58,7 @@ plot.close()
 
 fig = plot.figure()
 plot.plot(data_frame['date']+data_frame['hour'], data_frame['Switchグレー'], '.-k', label='Switch G')
-plot.xticks(days_plot, label_list, fontsize=8)
+plot.xticks(days_plot, label_list, fontsize=FONT_SIZE)
 plot.legend()
 fig.set_figwidth(FIG_W)
 plot.savefig('rudeya_Switch_G.png')
@@ -62,7 +66,7 @@ plot.close()
 
 fig = plot.figure()
 plot.plot(data_frame['date']+data_frame['hour'], data_frame['Switchネオン'], '.-r', label='Switch N')
-plot.xticks(days_plot, label_list, fontsize=8)
+plot.xticks(days_plot, label_list, fontsize=FONT_SIZE)
 plot.legend()
 fig.set_figwidth(FIG_W)
 plot.savefig('rudeya_Switch_N.png')
@@ -71,7 +75,7 @@ plot.close()
 fig = plot.figure()
 plot.plot(data_frame['date']+data_frame['hour'], data_frame['Switchグレー'], '.-k', label='Switch G')
 plot.plot(data_frame['date']+data_frame['hour'], data_frame['Switchネオン'], '.-r', label='Switch N')
-plot.xticks(days_plot, label_list, fontsize=8)
+plot.xticks(days_plot, label_list, fontsize=FONT_SIZE)
 plot.legend()
 fig.set_figwidth(FIG_W)
 plot.savefig('rudeya_Switch.png')
